f873086e5e4cd1326064a5546647fd72fba92efd
Mukul Bhadu <mukulbhaduwork@gmail.com>
2025-10-26 22:50:49 +0530

    redefined classes and desgns

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
-import './App.css'
+
 const App = () => {
   return (
-    <div className='main flex  justify-center '>
-      <div className="left  w-2/3 bg-black text-white">
-        <h1 className='text-5xl font-bold mt-80 ml-48 mb-5'>Hi I’m Mukul,</h1>
-        <p className='  text-xl mr-4 mb-16 ml-48'>myself Mukul Bhadu, a passionate coding student who loves turning ideas into interactive, functional, and visually appealing digital experiences. Here, you’ll find a collection of my projects, experiments, and creative work that reflect my journey in learning and building with code. Explore my work, discover my skills, and feel free to connect — I’m always excited to collaborate and learn something new!</p>
-        <div className='text-2xl font-semibold text-amber-50 ml-48'>
-          Exploring all things tech, but I've definitely got a taste for front-end development and building intuitive user experiences.
-        </div>
+    <body>
 
-        <div className="skills mt-44 border-t  border-gray-900">
+
+      <div className='first flex flex-row space-x-40 bg-black border-b border-gray-900 pb-14 '>
+        <div className="left   bg-black text-white max-w-1/2">
+          <h1 className='text-5xl font-bold mt-80 ml-48 mb-5'>Hi I’m Mukul,</h1>
+          <p className='  text-xl mr-4 mb-16 ml-48'>myself Mukul Bhadu, a passionate coding student who loves turning ideas into interactive, functional, and visually appealing digital experiences. Here, you’ll find a collection of my projects, experiments, and creative work that reflect my journey in learning and building with code. Explore my work, discover my skills, and feel free to connect — I’m always excited to collaborate and learn something new!</p>
+          <h1 className='text-2xl font-semibold text-amber-50 ml-48'>
+            Exploring all things tech, but I've definitely got a taste for front-end development and building intuitive user experiences.
+          </h1> </div>
+        <div className="right  bg-black text-white"><img src="https://pbs.twimg.com/profile_images/1982407747640410112/Vx6vADm8_400x400.jpg" alt=" picture" className=" mt-80 rounded-full w-" />
+          <div className="adddetails mt-6 ml-16">
+            <span className="location">📍Sriganganagar, Rajasthan, India</span> <h3 className="hover:text-amber-50 hover:cursor-pointer mt-0.5">Mukulbhaduwork@gmail.com</h3> <h1 className="text-xl font-bold mt-4">Education</h1> <ul> <li>Engineering college Bikaner</li> <li> Btech : 2024-2028</li> <li>CSE</li> </ul>
+          </div>
+        </div>
+      </div>
+      <div className="second flex flex-row bg-black text-white pb-10 space-x-80 border-b border-gray-900 ">
+        <div className="skills">
           <h2 className='text-3xl font-bold justify-center  flex my-10'>Skills</h2>
-          <ul className="flex ml-48 gap-16 ">
+          <ul className="flex space-x-16 ml-20 ">
             <li className=" hover:scale-110 flex flex-col items-center">
               <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" className=" w-16 h-16" alt="HTML5 Logo" />
               <p className="mt-2 font-semibold text-lg text-white">HTML5</p>
@@ -41,28 +50,8 @@ const App = () => {
               <p className="mt-2 font-semibold text-lg text-white">React</p>
             </li>
           </ul>
-          <h1 className="text-3xl font-bold justify-center  flex my-20">Learning</h1>
-          <ul className="flex justify-center ml-48 gap-16 mb-20"><li className="flex flex-col  hover:scale-110 transition-transform duration-300">
-            <svg className="w-16 h-16" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
-              <path fill="#47A248" d="M64 0C28.7 0 0 28.7 0 64s28.7 64 64 64 64-28.7 64-64S99.3 0 64 0z" />
-              <path fill="#FFF" d="M64 12c-11 0-20 9-20 20 0 11 20 48 20 48s20-37 20-48c0-11-9-20-20-20z" />
-            </svg>
-            <p className="mt-2 font-semibold text-lg text-white">MongoDB</p>
-          </li></ul>
         </div>
-      </div>
-      <div className="right  w-1/3 bg-black text-white h-auto">
-         <img src="https://pbs.twimg.com/profile_images/1982407747640410112/Vx6vADm8_400x400.jpg" alt=" picture" className=" mt-60 mb-1 ml-0 rounded-full w-96" />
-      
-        <span className="location">📍Sriganganagar, Rajasthan, India</span>
-        <h3 className="hover:text-amber-50 hover:cursor-pointer mt-0.5">Mukulbhaduwork@gmail.com</h3>
-        <h1 className="text-xl font-bold mt-4">Education</h1>
-        <ul>
-          <li>Engineering college Bikaner</li>
-          <li> Btech : 2024-2028</li>
-          <li>CSE</li>
-        </ul>
-        <div className="programing mt-10 border border-gray-900 ">
+        <div className="programing">
           <h2 className="text-3xl font-bold justify-center  flex mt-8">Programming Languages</h2>
           <ul className='flex gap-16 mb-10 mt-10 justify-center'>
             <li className='hover:scale-110'><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg" class="w-16 h-16" alt="C Logo" />
@@ -73,8 +62,20 @@ const App = () => {
               <p className="mt-2 font-semibold text-lg text-white">Python</p></li>
           </ul>
         </div>
-        <div className="others">
-          <h2 className="text-3xl font-bold justify-center  flex mt-8">Version Control & Collaboration</h2>
+      </div>
+      <div className="third flex flex-row space-x-96 bg-black text-white pt-10 ">
+        <div className="learning ml-52">
+          <h1 className="text-3xl font-bold justify-center ml-32 flex ">Learning</h1>
+          <ul className="ml-40 mt-20"><li className="flex flex-col  hover:scale-110 transition-transform duration-300">
+            <svg className="w-16 h-16" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
+              <path fill="#47A248" d="M64 0C28.7 0 0 28.7 0 64s28.7 64 64 64 64-28.7 64-64S99.3 0 64 0z" />
+              <path fill="#FFF" d="M64 12c-11 0-20 9-20 20 0 11 20 48 20 48s20-37 20-48c0-11-9-20-20-20z" />
+            </svg>
+            <p className="mt-2 font-semibold text-lg text-white">MongoDB</p>
+          </li></ul>
+        </div>
+        <div className="Version Control & Collaboration">
+           <h2 className="text-3xl font-bold justify-center  flex ">Version Control & Collaboration</h2>
           <ul className='flex gap-16 mb-10  justify-center mt-20'> <li className="flex flex-col items-center hover:scale-110 transition-transform duration-300">
             <svg className="w-16 h-16" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg">
               <path fill="#F34F29" d="M124.1 58.2L69.8 3.9a9.5 9.5 0 0 0-13.4 0l-10.6 10.6 17 17a7.4 7.4 0 0 1 9.4 9.4l16.4 16.4a7.4 7.4 0 1 1-4.6 4.6L67.6 45v29.6a7.4 7.4 0 1 1-5.3-.2V44.8a7.4 7.4 0 0 1-4.9-9.4L40.2 18.1 3.9 54.4a9.5 9.5 0 0 0 0 13.4l54.3 54.3a9.5 9.5 0 0 0 13.4 0l52.5-52.5a9.5 9.5 0 0 0 0-13.4z" />
@@ -92,7 +93,10 @@ const App = () => {
           </ul>
         </div>
       </div>
-    </div>
+
+
+    </body>
+
 
   )
 }

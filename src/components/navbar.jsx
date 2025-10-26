225b085f48e797a5ef96b7112c9d7833c264412f
Mukul Bhadu <mukulbhaduwork@gmail.com>
2025-10-26 20:47:38 +0530

    added rensponsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
-import '../index.css'
-import '../App.css'
+import { useState } from 'react';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className='bg-zinc-950 text-white flex justify-around  h-20 pt-6 fixed w-full border-b border-gray-900 top-0 z-10'>
-      <div className="left text-3xl font-bold text-amber-50 hover:cursor-pointer">Mukul's Portfolio</div>
-      <div className="right flex"><ul className='flex space-x-6 '>
-        <a href="https://github.com/mukulbhadu" class="hover:text-gray-400 transition">GitHub</a>
-        <a href="https://www.linkedin.com/in/mukulbhadu/" class="hover:text-gray-400 transition">LinkedIn</a>
-        <a href="https://www.instagram.com/cmbx1/" class="hover:text-gray-400 transition">Instagram</a>
-      </ul>
-      </div>
+    <>
 
-    </div>
-  )
-}
+      <nav className='bg-zinc-950 text-white flex space-x-96 items-center h-20 px-6 fixed w-full border-b border-gray-900 top-0 z-20'>
 
-export default Navbar
 
+        <div className="left text-3xl font-bold ml-40 text-amber-50 hover:cursor-pointer">
+          Mukul's Portfolio
+        </div>
+
+
+        <div className="right">
+
+
+          <ul className='hidden md:flex  gap-8 text-lg'>
+            <li>
+              <a href="https://github.com/mukulbhadu" className="hover:text-gray-400 transition">GitHub</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/mukulbhadu/" className="hover:text-gray-400 transition">LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/cmbx1/" className="hover:text-gray-400 transition">Instagram</a>
+            </li>
+          </ul>
+
+
+
+        </div>
+      </nav>
+
+
+
+    </>
+  );
+};
+
+export default Navbar;

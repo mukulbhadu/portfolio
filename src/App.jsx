cb68ce03d7ef0d950733073fab4aef1a07d740b1
Mukul bhadu <mukulbhadu222@gmail.com>
2025-10-26 16:33:59 +0530

    Update image source path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ const App = () => {
         </div>
       </div>
       <div className="right  w-1/3 bg-black text-white h-auto">
-        <img src="./1.png" alt="Image" className=" mt-72 mb-3.5 ml-0 rounded-full w-80" />
+        <img src="./public/1.png" alt="Image" className=" mt-72 mb-3.5 ml-0 rounded-full w-80" />
         <span className="location">📍Sriganganagar, Rajasthan, India</span>
         <h3 className="hover:text-amber-50 hover:cursor-pointer mt-0.5">Mukulbhaduwork@gmail.com</h3>
         <h1 className="text-xl font-bold mt-4">Education</h1>
